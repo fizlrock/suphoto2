408910b7cc8c32bb07dad4979dcbfe255f7067f1
fizlrock <nirku0soft@yandex.ru>
2024-07-05 18:23:27 +0400

    init

---
+++ src/main/java/dev/fizlrock/suphoto/SuphotoApplication.java
@@ -0,0 +1,16 @@
+
+ 
+
+package dev.fizlrock.suphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuphotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuphotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fizlrock/suphoto/SuphotoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fizlrock.suphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuphotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
