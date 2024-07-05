035c471a2be8c43951e00a4b0cc9b0f167ce33b1
fizlrock <nirku0soft@yandex.ru>
2024-07-05 18:35:19 +0400

    test openapi classes

--- src/main/java/dev/fizlrock/suphoto/SuphotoApplication.java
+++ src/main/java/dev/fizlrock/suphoto/SuphotoApplication.java
@@ -6,11 +6,18 @@ package dev.fizlrock.suphoto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.openapitools.api.UsersApi;
+
 @SpringBootApplication
 public class SuphotoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuphotoApplication.class, args);
+	}
+
+
+	class someApi implements UsersApi{
+		
 	}
 
 }
